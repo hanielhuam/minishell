421e0f69c327bbbd319e11f074971c94c5da8d91
Haniel Huam <hanielhuam@hotmail.com>
2025-08-23 17:45:22 -0300

    WIP: iniciando a transformação das variáveis de ambiente para lista

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hmacedo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 18:44:17 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/08/22 21:58:58 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/08/23 17:14:45 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,19 @@
 # include <readline/history.h>
 # include "ft_printf.h"
 
+typedef struct s_env
+{
+	char	*key;
+	char	*value;
+}			t_env;
+
 typedef struct t_shell
 {
-	t_list *env;
-	t_list *tokens;
+	t_list **env;
+	t_list **tokens;
 }
 
 int	get_input(char	**imput);
-t_list	*get_env_list(char **env);
+t_list	**get_env_list(char **env);
 
 #endif
--- src/get_env.c
+++ src/get_env.c
@@ -6,8 +6,29 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 21:57:03 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/08/22 21:57:22 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/08/23 17:43:14 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
+t_list	**get_env_list(char **env)
+{
+	t_list	**list;
+
+	if (!env || !*env)
+	{
+		ft_putstr_fd(STDERR_FILENO, "Enviroment variables ate empty");
+		return (NULL);
+	}
+	list = malloc(sizeof(t_list *));
+	if (!list)
+	{
+		ft_putstr_fd(STDERR_FILENO, "Error while alloc list pointer");
+		return (NULL);
+	}
+	while (*env)
+	{
+		
+	}
+}
