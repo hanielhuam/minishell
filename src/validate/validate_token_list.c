0aef09851ced2f8a1c2df8f9a2c20e552a390c6f
Haniel Huam <hanielhuam@hotmail.com>
2025-09-20 23:28:50 -0300

    iniciando os trabalhos da análise sintática

--- src/validate/validate_token_list.c
+++ src/validate/validate_token_list.c
@@ -6,14 +6,26 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 15:06:14 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/09/19 17:51:03 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/09/20 23:27:50 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int (*get_validation_functions(void)
+
+static int	validate_token(t_token *token)
+{
+	
+}
+
 int	validate_token_list(t_dlist *tokens)
 {
-	(void)tokens;
+	while (tokens)
+	{
+		if (validate_token(tokens->content))
+			return (1);
+		tokens = tokens->next;
+	}
 	return (0);
 }
