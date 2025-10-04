725446cd593144d52443300f659f074efce0b3b9
hmacedo- <hanielhuam42rio@outlook.com>
2025-10-03 21:26:40 -0300

    está quase chegando lá, fata pouco para validar o subshell

--- src/validate/validate_subshell.c
+++ src/validate/validate_subshell.c
@@ -6,7 +6,7 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 20:35:46 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/09/27 20:38:35 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/10/03 21:25:05 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 int	validate_subshell(t_dlist *tokens)
 {
-	(void)tokens;
-	return (0);
+	int	diferent_tokens;
+
+	diferent_tokens = 0;
+	while (tokens)
+	{
+		if (((t_token *)tokens->content)->type == TK_SUBSHELL)
+		{
+			if (!validate_subshell(*(((t_token *)tokens->content)->subshell)))
+				return (0);
+		}
+		else
+			diferent_tokens++;
+		tokens = tokens->next;
+	}
+	if (!diferent_tokens)
+		show_error("Syntax Error: don't allow only subshell inside subshel\n");
+	return (diferent_tokens);
 }
