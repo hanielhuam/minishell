0a13cf0b86a3d0cee54494a23c5f60a5c13202d2
Haniel Huam <hanielhuam@hotmail.com>
2025-10-12 18:24:51 -0300

    acho que hoje eu ainda termino essa parte de montar a árvore com o and e o or

--- src/handler/find_and_or.c
+++ src/handler/find_and_or.c
@@ -6,27 +6,56 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 19:53:07 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/10/11 21:31:22 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/10/12 18:23:45 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_dlist	*find_and_or_left(t_dlist *init, t_dlist *end, int l_or_r)
+static t_dlist	*increment_direction(t_dlist *token, int l_or_r)
 {
 	if (l_or_r)
-		init = init->next;
-	else
-		init = init->before;
-	while (init && init != end)
+		return (token->next);
+	return (token->before);
+}
+
+static int	check_token_type(t_dlist *token, t_tok_type *types)
+{
+	t_toke_type	value;
+
+	value = ((t_token *)token->content)->type;
+	while (*types != TK_NO_TYPE)
 	{
-		if (((t_token *)init->content)->type == TK_AND || \
-				((t_token *)init->content)->type == TK_OR)
-			return (init);
-		if (l_or_r)
-			init = init->next;
-		else
-			init = init->before;
+		if (value == *types)
+			return (1);
+		types++;
+	}
+	return (0);
+}
+
+static t_dlist	*find_and_or_left(t_tree *init, t_tree *end, int l_or_r)
+{
+	t_dlist	*first;
+	t_dlist	*last;
+	static t_tok_type	types[2] = {TK_AND, TK_OR, TK_NO_TYPE};
+
+	first = ((t_data_tree *)init->content)->token;
+	if (!end)
+		last = NULL;
+	else
+		last = ((t_data_tree *)end->content)->token;
+	first = increment_direction(first, l_or_r);
+	while (fisrt && first != last)
+	{
+		if (check_token_type(first, types))
+		{
+			if (!end)
+				return (first);
+			else if (first == find_and_or_left(init, end->prev, l_or_r))
+				return (first);
+			return (NULL);
+		}
+		first = increment_direction(first, l_or_r);
 	}
 	return (NULL);
 }
