0f665061f0a75cd9dfe16d8cd6ca4c8866f2f057
hmacedo- <hanielhuam42rio@outlook.com>
2025-11-15 21:20:19 -0300

    vou terminar de configurar o pipe e vamos ver em casa se os testes vão passar

--- src/processor/pipe_processor.c
+++ src/processor/pipe_processor.c
@@ -6,35 +6,70 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 22:54:53 by hmacedo-          #+#    #+#             */
-/*   Updated: 2025/11/15 17:49:23 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2025/11/15 21:19:18 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	pipe_processor(t_tree *node, t_shell *shell, int dir)
+static int	config_pipe(t_pipe *pipe, t_tree *node, int dir)
 {
-	int		fds[2];
+
+}
+
+static void	finalize_pipe(t_pipe *pipe)
+{
+	close(pipe->fds[0]);
+	close(pipe->fds[1]);
+	free(pipe);
+}
+
+static t_pipe	*generate_pipe(void)
+{
 	t_pipe	*pipe;
+	int		fds[2];
 
 	if (pipe(fds))
 	{
 		show_error("Error when open pipe\n");
-		return (-1);
+		return (NULL);
 	}
 	pipe = create_t_pipe(fds[0], fds[1]);
 	if (!pipe)
 	{
-		close(fds[0]);
-		close(fds[1]);
-		return (-1);
+		finalize_pipe(pipe, fds);
+		return (NULL);
 	}
+	return (pipe);
+}
+
+int pipe_execution(t_tree *node, t_shell *shell)
+{
+	int	ex1;
+	int	ex2;
+
+	ex1 = execution_command_tree(node->left, shell, 0);
+	close(((t_data_tree *)node->content)->pipe->fds[1]);
+	ex2 = execution_command_tree(node->right, shell, 1);
+	close(((t_data_tree *)node->content)->pipe->fds[0]);
+	if (ex1 || ex2)
+		return (-1);
+	if (wait_check(node))
+		return (wait(process));
+	return (0);
+}
+
+int	pipe_processor(t_tree *node, t_shell *shell, int dir)
+{
+	t_pipe	*pipe;
+
+	pipe = generate_pipe();
+	if (!pipe)
+		return (-1);
 	if (config_pipes(node, pipe, dir))
 	{
-		free(pipe);
-		close(fds[0]);
-		close(fds[1]);
+		finalize_pipe(pipe);
 		return (-1);
 	}
-	return (0);
+	return (pipe_execution(node, shell));
 }
