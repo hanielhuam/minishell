587b0745c460bc443db8fc331033f0ae7fa1a44a
hmacedo- <hanielhuam42rio@outlook.com>
2025-11-17 21:21:25 -0300

    esqueci de adicionar essas 2 modificações

---
+++ src/built_in/command_cd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_cd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 19:23:33 by hmacedo-          #+#    #+#             */
+/*   Updated: 2025/11/17 21:20:43 by hmacedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	change_pwd(t_list **env)
+{
+	char	*dir;
+
+	dir = getcwd(NULL, 0);
+	if (!dir)
+	{
+		perror("minishell: ");
+		return (1);
+	}
+
+}
+
+int	command_cd(t_command *command, t_list **env, int fd)
+{
+	(void)fd;
+	if (chdir(command->cmd_args[1]))
+	{
+		perror("minishell: ");
+		return (1);
+	}
+	if (change_pwd(env))
+		return (1);
+	return (0);
+}
